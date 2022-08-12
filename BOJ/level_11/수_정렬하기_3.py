0eea4f41ebaa84eca9103fc6406c1e881d523805
leadawon <72075335+leadawon@users.noreply.github.com>
2022-08-12 20:33:07 +0900

    Create 수_정렬하기_3.py

---
+++ BOJ/level_11/수_정렬하기_3.py
@@ -0,0 +1,46 @@
+'''
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+예제 입력 1 
+10
+5
+2
+3
+1
+4
+2
+3
+5
+1
+7
+예제 출력 1 
+1
+1
+2
+2
+3
+3
+4
+5
+5
+7
+'''
+import sys
+n=int(sys.stdin.readline())
+arr=[0]*(10001)
+
+for i in range(n):
+    arr[int(sys.stdin.readline())]+=1
+#arr.sort(reverse=False)
+
+for i in range(10001):
+    for j in range(arr[i]):
+        sys.stdout.write(str(i))
+        sys.stdout.write("\n")
